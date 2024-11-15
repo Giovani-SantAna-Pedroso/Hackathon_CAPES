798cabd647fd5c1bdcca8764a2d519ddef2f6354
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-15 17:26:34 -0300

    adicionando audio

--- src/app/api/coletalinks_route/route.js
+++ src/app/api/coletalinks_route/route.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs');
+
 import { ColetaLinkDeSite } from "@/Context/Context";
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
@@ -6,27 +8,32 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 export const GET = async () => {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const result = await model.generateContent(["ola"]);
+  const result = await model.generateContent(["ola tuo bem, quem [e voc"]);
   const linkColetado = result.response.text();
+  synthesizeText(linkColetado) 
 
-  convertTextToSpeech("Hello, this is a test!");
+
   return Response.json({ msg: linkColetado }, { status: 200 });
 };
 
 
+const url = 'http://localhost:5000/synthesize'; // Endereço da sua API
 
-async function convertTextToSpeech(text) {
+// Função para enviar texto e receber o áudio
+async function synthesizeText(text) {
   try {
-    const response = await axios.post('https://api.luvvoice.com/convert', {
-      text: text,
-      voice: 'pt-BR',
-      format: 'mp3', 
-    });
+    // Faz a requisição POST para a API
+    const response = await axios.post(url, { text }, { responseType: 'arraybuffer' });
 
-    // Processar o áudio retornado
-    console.log('Audio URL:', response.data.audio_url);
+    // Salva o arquivo de áudio recebido
+    fs.writeFileSync('audio/audio.mp3', response.data);
+
+    console.log('Áudio salvo como "audio.mp3"');
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error('Erro ao fazer a requisição:', error.response ? error.response.data : error.message);
   }
 }
 
+const texto = 'Olá, esse é um exemplo de áudio sintetizado!';
+
+synthesizeText(texto);
