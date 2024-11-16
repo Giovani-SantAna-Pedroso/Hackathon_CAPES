41b5ada48b68c84981e225ef8fdb4e28cd292e0d
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-16 18:52:14 -0300

    Adicionando dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./dashboard.module.css"
+const dashboard = () => {
+  return (
+    <div className={styles.containerDashboard}>
+      
+      
+    </div>
+  )
+}
+
+export default dashboard
