0030867a9bb78a4549bccb17a575e2cec5822d0d
rafaelrvs <rafaelvieira1333@gmail.com>
2024-11-16 10:31:30 -0300

    Adicionando animação

--- src/app/Hero/index.jsx
+++ src/app/Hero/index.jsx
@@ -67,15 +67,19 @@ function Hero() {
           Pesquisa Avançada {showAdvancedSearch ? <ArrowUp /> : <ArrowDown />}
         </button>
 
-        {/* Advanced Search Section */}
-        {showAdvancedSearch && (
+        {/* Advanced Search Section with Animation */}
+        <div
+          className={`overflow-hidden transition-all duration-500 ease-in-out ${
+            showAdvancedSearch ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+          }`}
+        >
           <div className="mt-4">
             <AdvancedSearch
               inputValue={inputValue}
               setInputValue={setInputValue}
             />
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
